d8be70d42a377c61917f0b19a7c125f5754b4bb9
Johan Palmfjord <johan.palmfjord@gmail.com>
2016-10-23 11:19:32 +0200

    Add compile class test

--- src/test/java/org/twig/syntax/parser/node/ModuleTests.java
+++ src/test/java/org/twig/syntax/parser/node/ModuleTests.java
@@ -1,10 +1,28 @@
 package org.twig.syntax.parser.node;
 
 import org.junit.Test;
+import org.twig.compiler.ClassCompiler;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
 
 public class ModuleTests {
     @Test
-    public void canCompileClassHeader() {
-        // TODO with mocked compiler
+    public void canCompileEmptyClass() {
+        ClassCompiler classCompilerStub = mock(ClassCompiler.class);
+
+        Module module = new Module(new ArrayList<>());
+        module.setFileName("foo");
+
+        module.compile(classCompilerStub);
+
+        verify(classCompilerStub).writeLine("package org.twig.template;\n");
+        verify(classCompilerStub).writeLine("/**");
+        verify(classCompilerStub).writeLine(" * foo");
+        verify(classCompilerStub).writeLine(" */");
+        verify(classCompilerStub).write("public class TODO"); // TODO
+        verify(classCompilerStub).writeLine(" extends BASECLASS {"); // TODO
+        verify(classCompilerStub).indent();
     }
 }
