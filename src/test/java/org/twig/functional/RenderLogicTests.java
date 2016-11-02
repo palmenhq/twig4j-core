c022aedb17c8ebabc02e7b91eeac30fa96c2c290
Johan Palmfjord <johan.palmfjord@gmail.com>
2016-11-02 22:34:48 +0100

    Add a functional test suite for logic

---
+++ src/test/java/org/twig/functional/RenderLogicTests.java
@@ -0,0 +1,18 @@
+package org.twig.functional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.twig.exception.TwigException;
+
+import java.util.HashMap;
+
+public class RenderLogicTests extends FunctionalTests {
+    @Test
+    public void canRenderPlainBoolean() throws TwigException {
+        HashMap<String, String> templates = new HashMap<>();
+        templates.put("foo.twig", "{{ true }}");
+        setupEnvironment(templates);
+
+        Assert.assertEquals("Bool should render text bool", "true", environment.render("foo.twig"));
+    }
+}
