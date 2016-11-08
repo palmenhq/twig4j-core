e7447a0580d3c9f236dc40f1f1c9f1d9e0b9968d
Johan Palmfjord <johan.palmfjord@gmail.com>
2016-11-08 22:06:48 +0100

    Add test for basic if statements

---
+++ src/test/java/org/twig/functional/RenderControlStructuresTests.java
@@ -0,0 +1,34 @@
+package org.twig.functional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.twig.exception.TwigException;
+import org.twig.exception.TwigRuntimeException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RenderControlStructuresTests extends FunctionalTests {
+    @Test
+    public void canRenderIf() throws TwigException {
+        HashMap<String, String> templates = new HashMap<>();
+        templates.put(
+                "foo.twig",
+                "{% if foo %}foo{% endif %}\n"
+                + "{% if bar %}bar{% endif %}\n"
+        );
+        setupEnvironment(templates);
+
+        HashMap<String, Boolean> ctx = new HashMap<>();
+        ctx.put("foo", true);
+        ctx.put("bar", false);
+
+        Assert.assertEquals(
+                "Only contents of true if statement should be rendered",
+                "foo\n\n",
+                environment.render("foo.twig", ctx)
+        );
+    }
+}
