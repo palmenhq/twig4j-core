454be7836fe3309feb0d1127b7786ad7d5b4b825
Johan Palmfjord <johan.palmfjord@gmail.com>
2017-01-24 22:28:21 +0100

    Always type cast to int when using binary mod

--- src/main/java/org/twig/syntax/parser/node/type/expression/BinaryMod.java
+++ src/main/java/org/twig/syntax/parser/node/type/expression/BinaryMod.java
@@ -1,11 +1,28 @@
 package org.twig.syntax.parser.node.type.expression;
 
 import org.twig.compiler.ClassCompiler;
+import org.twig.exception.LoaderException;
+import org.twig.exception.TwigRuntimeException;
 import org.twig.syntax.parser.node.Node;
 
 public class BinaryMod extends Binary {
     public BinaryMod(Node left, Node right, Integer line) {
         super(left, right, line);
+    }
+
+    @Override
+    public void compile(ClassCompiler compiler) throws LoaderException, TwigRuntimeException {
+        compiler
+                .writeRaw("((Integer)")
+                .subCompile(getLeftNode())
+                .writeRaw(" ");
+
+        compileOperator(compiler);
+
+        compiler
+                .writeRaw(" ((Integer)")
+                .subCompile(getRightNode())
+                .writeRaw("))");
     }
 
     @Override
